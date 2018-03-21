45e80e6ba3494ad6e028df3129f1560749d4ba0e
Jamie Jones <jbjonesjr@github.com>
2018-03-21 16:16:20 -0400

    fixes #2

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ module.exports = (robot) => {
   // https://probot.github.io/docs/development/
 
   robot.on('push', push)
+  robot.on('status', commit_status)
 
   // event(s): push
   async function push(context) {
@@ -20,6 +21,28 @@ module.exports = (robot) => {
     } else {
       // usecase: on a push to the master branch...
       robot.log('swallow non-master pushes')
+    }
+  }
+
+  // event(s): status
+  async function commit_status(context) {
+    if (context.payload.branches[0].name === context.payload.repositories.master_branch) {
+      robot.log('commit status received for `', context.payload.context, '`, state: ', context.payload.state, 'on branch: ', context.payload.branches[0].name, 'trace')
+
+      // don't care what the status tool was,
+      // just that it might meet the criteria
+      if (context.payload.state === 'success') {
+        context.github.repos.createDeployment(context.repo({
+          'ref': context.payload.after
+        }))
+      }
+
+      context.github.repos.createDeployment(context.repo({
+        'ref': context.payload.after
+      }))
+    } else {
+      // usecase: on a successful commit status to the master branch...
+      robot.log('swallow non-master commit statuses')
     }
   }
 }
