6f3efe708f17fbaebd16a9dbe64514d0593a55da
Jamie Jones <jbjonesjr@github.com>
2018-03-21 15:32:42 -0400

    fixes #1

--- index.js
+++ index.js
@@ -7,4 +7,19 @@ module.exports = (robot) => {
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
+
+  robot.on('push', push)
+
+  // event(s): push
+  async function push (context) {
+    if (context.payload.ref.split('\\')[2] === context.payload.repositories.master_branch) {
+      console.log('push to the master branch')
+      context.github.repos.createDeployment(context.repo({
+        'ref': context.payload.after
+      }))
+    } else {
+      // usecase: on a push to the master branch...
+      console.log('swallow non-master pushes')
+    }
+  }
 }
