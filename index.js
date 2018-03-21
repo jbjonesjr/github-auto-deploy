ff2a28e4692b9245fe0964a08f65944c093dc682
Jamie Jones <jbjonesjr@github.com>
2018-03-21 16:17:13 -0400

    add additional logging

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ module.exports = (robot) => {
   // event(s): push
   async function push(context) {
     if (context.payload.ref.split('\\')[2] === context.payload.repositories.master_branch) {
-      robot.log('push to the master branch')
+      robot.log('push to the branch: ', context.payload.ref.split('\\')[2])
       context.github.repos.createDeployment(context.repo({
         'ref': context.payload.after
       }))
